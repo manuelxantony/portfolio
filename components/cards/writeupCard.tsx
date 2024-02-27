0adc4ab5fc66df6a14aab40d49ea9346ba06ea43
manuelxantony <manuelxantony@gmail.com>
2024-02-27 23:57:58 +0530

    changed style in writeupcard

--- components/cards/writeupCard.tsx
+++ components/cards/writeupCard.tsx
@@ -7,7 +7,7 @@ import { Writeup } from '@/app/lib/definitions';
 export default function WriteupCard({ writeup }: { writeup: Writeup }) {
   return (
     <div className="border-b bottom-1 h-16 p-4">
-      <div className="flex flex-row justify-between">
+      <div className="flex flex-row flex-wrap justify-between">
         <div className="flex flex-row flex-wrap gap-4 ">
           <div className="flex justify-center items-center">
             <ArrowTopRightIcon />
@@ -16,7 +16,7 @@ export default function WriteupCard({ writeup }: { writeup: Writeup }) {
             <h4 className="font-medium">{writeup.title}</h4>
           </div>
         </div>
-        <div className="text-gray-500 ">{writeup.date}</div>
+        <div className="text-gray-500 w-24 bg-yellow-300">{writeup.date}</div>
       </div>
     </div>
   );
